d55972506592ccfb5668cd7d508c0d62cc6803c0
Michael Burke <michael_burke4@student.uml.edu>
2023-03-05 15:45:33 -0500

    add a .clang-format & make rule, format main

--- Makefile
+++ Makefile
@@ -4,8 +4,9 @@ BIN = controller
 
 $(BIN): main.o
 	$(CC) $< -o $@ $(CFLAGS)
-	
 %.o: %.c
 	$(CC) -c $< -o $@ $(CFLAGS)
+format:
+	clang-format -i -style=file *.[ch]
 clean:
 	rm *.o $(BIN)
--- main.c
+++ main.c
@@ -1,20 +1,19 @@
-#include <signal.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <err.h>
 #include <errno.h>
-#include <string.h>
 #include <fcntl.h>
-#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/prctl.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #define READ_END 0
 #define WRITE_END 1
 #define BUFFSIZE 2048
 #define SERVER_MSG_SIZE 33
-
 
 struct user_msg {
 	char *time;
@@ -36,14 +35,12 @@ int main(void)
 	char buffer[BUFFSIZE];
 	ssize_t num_read;
 
-
-	argv = malloc(5 * sizeof (char *));
+	argv = malloc(5 * sizeof(char *));
 	argv[0] = "java";
 	argv[1] = "-jar";
 	argv[2] = "server.jar";
 	argv[3] = "nogui";
 	argv[4] = NULL;
-
 
 	ppid_before_fork = getpid();
 	if (pipe(server_input) == -1)
@@ -59,7 +56,7 @@ int main(void)
 		int r;
 		dup2(server_input[READ_END], STDIN_FILENO);
 		dup2(server_output[WRITE_END], STDOUT_FILENO);
-		
+
 		r = prctl(PR_SET_PDEATHSIG, SIGTERM);
 		if (r == -1)
 			err(1, "parent process exited before prctl...");
@@ -68,7 +65,7 @@ int main(void)
 		if (getppid() != ppid_before_fork)
 			err(1, "child process was adopted. not continuing...");
 		if (execvp(argv[0], argv))
-		 	err(1, "bad exec");
+			err(1, "bad exec");
 	}
 	free(argv);
 	close(server_input[READ_END]);
@@ -83,16 +80,17 @@ int main(void)
 		.message_space = 0
 	};
 
-	while((num_read = read(server_output[READ_END], buffer, BUFFSIZE))) {
+	while ((num_read = read(server_output[READ_END], buffer, BUFFSIZE))) {
 		printf("%s", buffer);
-		
-		if(0 == process(buffer, &msg)){
+
+		if (0 == process(buffer, &msg)) {
 			printf("%s said: %s", msg.user, msg.message);
 		}
-		for (int i = 0 ; i < num_read ; ++i) {
+		for (int i = 0; i < num_read; ++i) {
 			buffer[i] = '\0';
 		}
-		if(errno) printf("error: %s\n", strerror(errno));
+		if (errno)
+			printf("error: %s\n", strerror(errno));
 	}
 }
 
@@ -100,7 +98,7 @@ int main(void)
 
 int process(char *buffer, struct user_msg *msg_struct)
 {
-    if (buffer == NULL || buffer[0] != '[') {
+	if (buffer == NULL || buffer[0] != '[') {
 		return -1;
 	}
 
@@ -117,25 +115,26 @@ int process(char *buffer, struct user_msg *msg_struct)
 	}
 
 	size_t i; // index of the username
-	for (i = SERVER_MSG_SIZE; buffer[i] != '>'; i++);
+	for (i = SERVER_MSG_SIZE; buffer[i] != '>'; i++)
+		;
 	size_t user_message_size = i - SERVER_MSG_SIZE - 1;
 	if (user_message_size > msg_struct->user_space) {
 		printf("reallocing user pointer...\n");
-		msg_struct->user = realloc(msg_struct->user, (user_message_size+1) * sizeof(char));
-		msg_struct->user_space = user_message_size+1;
+		msg_struct->user = realloc(msg_struct->user, (user_message_size + 1) * sizeof(char));
+		msg_struct->user_space = user_message_size + 1;
 	}
-	strncpy(msg_struct->user, &(buffer[SERVER_MSG_SIZE+1]), user_message_size);
+	strncpy(msg_struct->user, &(buffer[SERVER_MSG_SIZE + 1]), user_message_size);
 	msg_struct->user[user_message_size] = '\0';
 
-
-	i+=2; // moves index from end of username to beginning of message
+	i += 2; // moves index from end of username to beginning of message
 	size_t j; // index of the end of the message statement
-	for (j = i; buffer[j] != '\0'; j++);
+	for (j = i; buffer[j] != '\0'; j++)
+		;
 	size_t server_msg_len = j - i;
 	if (server_msg_len > msg_struct->message_space) {
 		printf("reallocing message pointer...\n");
-		msg_struct->message = realloc(msg_struct->message, (server_msg_len+1) * sizeof(char));
-		msg_struct->message_space = (server_msg_len+1);
+		msg_struct->message = realloc(msg_struct->message, (server_msg_len + 1) * sizeof(char));
+		msg_struct->message_space = (server_msg_len + 1);
 	}
 	strncpy(msg_struct->message, &(buffer[i]), server_msg_len);
 	msg_struct->message[server_msg_len] = '\0';

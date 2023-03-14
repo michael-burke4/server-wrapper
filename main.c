3bd2aa664535e31f4708eae00e72af9e6ab00005
Michael Burke <michael_burke4@student.uml.edu>
2023-03-14 17:41:27 -0400

    refactor main a bit and run format

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-// #include "discbot.h"
 #include "mcserver.h"
 #include "puppet.h"
 #include <err.h>
@@ -18,15 +17,18 @@
 #define MC_TAG "<MC>"
 
 void set_read_nonblocking(int fd[]);
+void start_puppet(int in_pipe[], int out_pipe[], char *argv[], char *chdir_path);
 
 int main(void)
 {
 	struct timespec read_wait = { 0, 1000L };
-	pid_t ppid_before_fork, pid;
 	int server_input[2], server_output[2];
 	int discord_input[2], discord_output[2];
 	char buffer[BUFFSIZE + 1], discord_buffer[BUFFSIZE + 1];
 	ssize_t num_read;
+	// TODO: don't hardcode argv. Config file or something?
+	char *mc_argv[] = { "java", "-jar", "server.jar", "nogui", NULL };
+	char *disc_argv[] = { "node", "app.js", NULL };
 	struct user_msg msg = {
 		.time = NULL,
 		.user = NULL,
@@ -36,37 +38,17 @@ int main(void)
 		.message_space = 0
 	};
 
-	ppid_before_fork = getpid();
 	if (pipe(server_input) == -1)
 		err(1, "bad pipe!");
 	if (pipe(server_output) == -1)
 		err(1, "bad pipe!");
-	set_read_nonblocking(server_output);
-	pid = fork();
-	if (pid == -1)
-		err(1, "bad fork!");
-	if (!pid) { // in child process...
-		char *argv[] = {"java", "-jar", "server.jar", "nogui", NULL};
-		run_puppet_process(server_input, server_output, ppid_before_fork, argv, "./mc_server");
-	}
-	close(server_input[READ_END]);
-	close(server_output[WRITE_END]);
+	start_puppet(server_input, server_output, mc_argv, "./mc_server");
 
-	ppid_before_fork = getpid();
 	if (pipe(discord_input) == -1)
 		err(1, "bad pipe!");
 	if (pipe(discord_output) == -1)
 		err(1, "bad pipe!");
-	set_read_nonblocking(discord_output);
-	pid = fork();
-	if (pid == -1)
-		err(1, "bad fork!");
-	if (!pid) { // in child process...
-		char *argv[] = {"node", "app.js", NULL};
-		run_puppet_process(discord_input, discord_output, ppid_before_fork, argv, "./discord_bot");
-	}
-	close(discord_input[READ_END]);
-	close(discord_output[WRITE_END]);
+	start_puppet(discord_input, discord_output, disc_argv, "./discord_bot");
 
 	while (1) {
 		// FIXME: using this (old) implementation prints the occasional garbage char.
@@ -89,7 +71,6 @@ int main(void)
 		}
 
 		num_read = read(server_output[READ_END], buffer, BUFFSIZE);
-		// printf("ERRNO: %d\n", errno);
 		if (num_read <= 0)
 			continue;
 
@@ -97,9 +78,7 @@ int main(void)
 			printf("%s said: %s", msg.user, msg.message);
 		}
 		for (int i = 0; i < num_read; ++i) {
-			// band-aid fix to above FIXME
 			printf("%c", buffer[i]);
-			// printf("i: %d buffer[i]: %c %d\n", i, buffer[i], buffer[i]); // debug stuff ignore
 			buffer[i] = '\0';
 		}
 	}
@@ -108,4 +87,20 @@ int main(void)
 void set_read_nonblocking(int fd[])
 {
 	fcntl(fd[READ_END], F_SETFL, O_NONBLOCK);
+}
+
+void start_puppet(int in_pipe[], int out_pipe[], char *argv[], char *chdir_path)
+{
+	int ppid_before_fork = getpid();
+	pid_t pid;
+
+	set_read_nonblocking(out_pipe);
+
+	pid = fork();
+	if (pid == -1)
+		err(1, "bad fork!");
+	if (!pid) // in child process...
+		run_puppet_process(in_pipe, out_pipe, ppid_before_fork, argv, chdir_path);
+	close(in_pipe[READ_END]);
+	close(out_pipe[WRITE_END]);
 }
--- puppet.c
+++ puppet.c
@@ -9,26 +9,26 @@
 
 void close_both_ends(int fd[2])
 {
-    close(fd[0]);
-    close(fd[1]);
+	close(fd[0]);
+	close(fd[1]);
 }
 void run_puppet_process(int puppet_in[], int puppet_out[], pid_t parent_pid, char **argv, char *chdir_path)
 {
-    int r;
-    dup2(puppet_in[READ_END], STDIN_FILENO);
-    dup2(puppet_out[WRITE_END], STDOUT_FILENO);
-    close_both_ends(puppet_in);
-    close_both_ends(puppet_out);
+	int r;
+	dup2(puppet_in[READ_END], STDIN_FILENO);
+	dup2(puppet_out[WRITE_END], STDOUT_FILENO);
+	close_both_ends(puppet_in);
+	close_both_ends(puppet_out);
 
-    r = prctl(PR_SET_PDEATHSIG, SIGTERM);
-    if (r == -1)
-        err(1, "parent process exited before prctl");
-    if (chdir_path != NULL) {
-        if (chdir(chdir_path))
-            err(1, "could not change directory to provided path.");
-    }
-    if (getppid() != parent_pid)
-        err(1, "child process adopted, not continuing.");
-    if (execvp(argv[0], argv))
-        err(1, "bad exec");
+	r = prctl(PR_SET_PDEATHSIG, SIGTERM);
+	if (r == -1)
+		err(1, "parent process exited before prctl");
+	if (chdir_path != NULL) {
+		if (chdir(chdir_path))
+			err(1, "could not change directory to provided path.");
+	}
+	if (getppid() != parent_pid)
+		err(1, "child process adopted, not continuing.");
+	if (execvp(argv[0], argv))
+		err(1, "bad exec");
 }
